2fa6fdd6d2f19ca234589f0d78a6bd04800734ca
nengannishasetyawati <55216332+nengannishasetyawati@users.noreply.github.com>
2020-03-29 10:28:38 +0700

    Update h_child.h

--- Tubes Strukdat-Daftar Riwayat pendidikan/h_child.h
+++ Tubes Strukdat-Daftar Riwayat pendidikan/h_child.h
@@ -1,5 +1,29 @@
 #ifndef H_CHILD_H_INCLUDED
 #define H_CHILD_H_INCLUDED
+#define first(L) L.first
+#define last(L) L.last
+#define info(P) P->info
+#define next(P) P->next
+using namespace std;
+typedef struct elmListChild *address2;
+typedef string infotypeChild;
+struct elmListChild{
+    infotypeChild info;
+    address2 next;
+};
+struct list_Child{
+    address2 first;
+    address2 last;
+};
+void createList(list_Child &L);
+void createElmt(address2 &P, infotypeChild jenjang);
+void insertFirst(list_Child &L, address2 P);
+void insertLast(list_Child &L, address2 P);
+void insertAfter(list_Child &L, address2 Prec, address2 P);
+void deletefirst(list_Child &L, address2 &P);
+void deleteLast(list_Child &L, address2 &P);
+void deleteAfter(list_Child &L, address2 Prec, address2 &P);
+void printList(list_Child L);
 
 
 
