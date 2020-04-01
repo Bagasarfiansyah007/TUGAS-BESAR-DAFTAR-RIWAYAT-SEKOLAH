364ffe8f2b527955814c901330d43c6f089da96d
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2020-04-01 22:08:58 +0700

    Update h_child.cpp

--- Tubes Strukdat-Daftar Riwayat pendidikan/h_child.cpp
+++ Tubes Strukdat-Daftar Riwayat pendidikan/h_child.cpp
@@ -1,94 +1,120 @@
-#include <iostream>
-#include "child.h"
-void createList(list_Child &L){
+#include "h_child.h"
+void createListchild(list_Child &L){
     first(L)=NULL;
     last(L)=NULL;
 }
-void createElmt(address2 &P){
+void createElmtchild(address2 &P){
     string jenjang;
-    cin>>jenjang;
+
     P = new elmListChild;
-    info(P) = jenjang;
     next(P) = NULL;
+
+    cout << "================ Menambhakan data Riwayat Pendidikan ================ " << endl << endl;
+
+    cout << "* input Riwayat Pendidikan Alumni             : ";
+    cin >> jenjang;
+    info(P) = jenjang;
+    cout << endl;
 }
 
-void insertLast(list_Child &L, address2 P){
-    if(first(L)==NULL){
-        first(L)=P;
-        last(L)=P;
-    }else{
-        next(last(L))=P;
-        last(L)=P;
+void insertLastchild(list_Child &L, address2 P){
+
+    if (first(L) == NIL && last(L) == NIL) {
+        first(L) = P;
+        last(L) = P;
+        next(P) = first(L);
+    } else {
+        next(P) = first(L);
+        first(L) = P ;
+        next(last(L)) = first(L);
     }
 }
 
-void deletefirst(list_Child &L, address2 &P){
-    P=first(L);
-    first(L)=next(P);
-    P=NULL;
+void deletefirstchild(list_Child &L, address2 &P){
+    P =first(L);
+    first(L) = next(P);
+    next(last(L))= first(L);
+    next(P) = NIL;
 }
-void deleteLast(list_Child &L, address2 &P){
-     P = first(L);
-        while(next(next(P))!= NULL){
-            P = next(P);
-        }
-        last(L) = P;
-        P = next(last(L));
-        next(last(L))=NULL;
+void deleteLastchild(list_Child &L, address2 &P){
+    address2 q;
+    P = last(L);
+    q = first(L);
+
+    while (next(q) != P) {
+        q = next(q);
+    }
+
+    last(L) = q;
+    next(q) = first(L);
+    next(P) = NIL;
 
 }
-void deleteElmt(list_Child &L, address2 Prec){
+void deleteElmtchild(list_Child &L, address2 &P){
     string jenjang;
-    cout<<"jenjang yang ingin dihapus :"<<endl;
-    cin>>jenjang;
-    Prec = searchJenjang(L,jenjang);
-    address2 P,Q;
-    if(first(L)==NULL){
+    address2 prec;
+    if(prec == NULL){
         cout<<"list kosong"<<endl;
+        P = NIL;
     }else{
+        cout << "jika ingin membatalkan ketik 'tidak' " << endl << endl;
+        cout << "Masukan Data yang Ingin di hapus berdasarkan Jenjang : ";
+        cin>>jenjang;
 
-            if(Prec==NULL){
+        if (jenjang != "tidak") {
+
+            prec = searchJenjang(L,jenjang);
+
+            if(prec==NULL){
                 cout<<"data tidak ada"<<endl;
-            }else{
-                if(next(first(L))==NULL && next(last(L))==NULL){
+            } else if(next(first(L)) == NULL){
                     P=first(L);
                     first(L)=NULL;
                     last(L)=NULL;
-                }else if(Prec==first(L)){
-                    deletefirst(L,Prec);
-                }else if(Prec==last(L)){
-                    deleteLast(L,Prec);
-                }else{
-                    P=first(L);
-                    while(P!=Prec){
-                        P=next(P);
+                    cout << "data dengan nama '" << jenjang << "' Terhapus !!" << endl;
+            }else if(prec==first(L)){
+                    P = prec;
+                    deletefirstchild(L,P);
+                    cout << "data dengan nama '" << jenjang << "' Terhapus !!" << endl;
+            }else if(prec==last(L)){
+                    deleteLastchild(L,P);
+                    cout << "data dengan nama '" << jenjang << "' Terhapus !!" << endl;
+            }else{
+                    P=prec;
+                    prec = first(L);
+                    while(next(prec)!= P){
+                        prec = next(prec);
                     }
-                    if(P==Prec){
-                        Q = first(L);
-                        while(next(Q)!= P){
-                            Q=next(Q);
-                        }
-                        if(next(Q)==P){
-                            next(Q)=next(P);
-                            P=NULL;
-                        }
-                    }
+                    next(prec)=next(P);
+                    next(P)=NULL;
+                    cout << "data dengan nama '" << jenjang << "' Terhapus !!" << endl;
                 }
+            } else{
+                cout << endl;
+                cout << "anda keluar dari menu !!" << endl;
             }
-        }
-
     }
-void printList(list_Child L){
+}
+void printListchild(list_Child L){
     address2 P;
+    int i = 0;
     P = first(L);
-    while(P!=NULL){
-        cout<<info(P)<<", ";
+
+    cout << "=============================== Riwayat Pendidikan yang tersedia =============================== " << endl << endl;
+
+    while(next(P)!=first(L)){
+        i = i + 1;
+        cout << i << "." << info(P) << endl;
         P = next(P);
     }
+
+    i = i + 1;
+    cout << i << "." << info(P) << endl;
+
     cout<<endl;
 
 }
-address2 searchJenjang(list_Child L, infotypeChild jenjang){
+address2 searchJenjang(list_Child L, string jenjang){
 
     address2 P;
     if(first(L)!=NULL){
