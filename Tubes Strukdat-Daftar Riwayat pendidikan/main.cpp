f8341411d2b3132a8dfceb7c5e3ea0d5b8c71a36
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2020-04-01 22:13:29 +0700

    Update main.cpp

--- Tubes Strukdat-Daftar Riwayat pendidikan/main.cpp
+++ Tubes Strukdat-Daftar Riwayat pendidikan/main.cpp
@@ -1,12 +1,15 @@
-#include "h_parent.h"
-#include <stdlib.h>
+#include "h_relasi.h"
 int main()
 {
     listParent l;
+    list_Child lc;
+    listRelasi lr;
     address1 p;
+    address2 q;
+    address3 r;
     int i,j,x,pilihan;
     user datauser[n];
-    string username,password,kode;
+    string jenjang,username,password,kode;
 
     cout << "Selamat datang Di program penginputan data alumni " << endl;
     cout << "Silahkan  Login terlebih dahulu !!" << endl;
@@ -44,18 +47,34 @@ int main()
             }
                 if ( username == datauser[j].username && password == datauser[j].password) {
                     createlistparent(l);
+                    createListchild(lc);
                     system("CLS");
                     cout << "Hai selamat datang " << username << "!!!" << endl;
                     cout << "disini kamu bisa menginputkan data alumni" << endl << endl;
 
                     cout << "Pilih Menu yang ingin di inputkan" << endl;
                     cout << endl;
+                    cout << "================== INSERT DATA ==================" <<endl << endl;
                     cout << "1.Insert Data Alumni" << endl;
                     cout << "2.Insert Data Pendidikan" << endl;
-                    cout << "3.Hapus Data Alumni" << endl;
-                    cout << "4.Hapus Data Pendidikan" << endl;
-                    cout << "5.Cari Data" << endl;
-                    cout << "6.Tampil Data" << endl;
+                    cout << "3.Insert Pemilihan Jenjang Pendidikan" << endl << endl;
+                    cout << "=================== EDIT DATA ===================" <<endl << endl;
+                    cout << "4.Edit Data Alumni" << endl;
+                    cout << "5.Edit Data Pendidikan" << endl << endl;
+
+                    cout << "================== DELETE DATA ==================" <<endl << endl;
+
+                    cout << "6.Hapus Data Alumni" << endl;
+                    cout << "7.Hapus Data Pendidikan" << endl << endl;
+
+                    cout << "================== SEARCH DATA ==================" <<endl << endl;
+
+                    cout << "8.Cari Data" << endl << endl;
+
+                    cout << "=================== VIEW DATA ===================" <<endl << endl;
+
+                    cout << "9.Tampil Data" << endl;
+
                     cout << "0.keluar" << endl;
                     cout << endl;
                     cout << "pilihan : ";
@@ -69,17 +88,39 @@ int main()
                             system("PAUSE");
                         } else if (pilihan == 2) {
                             system("CLS");
+                            cout << endl;
+                            createElmtchild(q);
+                            insertLastchild(lc,q);
+                            cout << "data terinput" << endl << endl;
+                            system("PAUSE");
                         } else if (pilihan == 3) {
+                            system("CLS");
+                            inputrelasi(lr,l,lc);
+                            cout << endl;
+                            system("PAUSE");
+                        } else if (pilihan == 4) {
+                            printListRelasi(lr);
+                            system("PAUSE");
+                        } else if (pilihan == 5) {
+                            printListchild(lc);
+                            system("PAUSE");
+                        } else if (pilihan == 6) {
                             system("CLS");
                             tampildataparent(l);
                             cout << endl;
                             deleteelmtparent(l,p);
                             system("PAUSE");
-                        } else if (pilihan == 4) {
+                        } else if (pilihan == 7) {
                             system("CLS");
-                        } else if (pilihan == 5) {
-                            system("CLS");
-                        } else if (pilihan == 6) {
+                            printListchild(lc);
+                            cout << endl;
+                            deleteElmtchild(lc,q);
+                            system("PAUSE");
+                        } else if (pilihan == 8) {
+
+                        } else if (pilihan == 9) {
+
+                        } else if (pilihan == 10) {
 
                         }
 
@@ -89,14 +130,28 @@ int main()
 
                         cout << "Pilih Menu yang ingin di inputkan" << endl;
                         cout << endl;
+                        cout << "================== INSERT DATA ==================" <<endl << endl;
                         cout << "1.Insert Data Alumni" << endl;
                         cout << "2.Insert Data Pendidikan" << endl;
-                        cout << "3.Hapus Data Alumni" << endl;
-                        cout << "4.Hapus Data Pendidikan" << endl;
-                        cout << "5.Cari Data" << endl;
-                        cout << "6.Tampil Data" << endl;
+                        cout << "3.Insert Pemilihan Jenjang Pendidikan" << endl << endl;
+                        cout << "=================== EDIT DATA ===================" <<endl << endl;
+                        cout << "4.Edit Data Alumni" << endl;
+                        cout << "5.Edit Data Pendidikan" << endl << endl;
+
+                        cout << "================== DELETE DATA ==================" <<endl << endl;
+
+                        cout << "6.Hapus Data Alumni" << endl;
+                        cout << "7.Hapus Data Pendidikan" << endl << endl;
+
+                        cout << "================== SEARCH DATA ==================" <<endl << endl;
+
+                        cout << "8.Cari Data" << endl << endl;
+
+                        cout << "=================== VIEW DATA ===================" <<endl << endl;
+
+                        cout << "9.Tampil Data" << endl;
+
                         cout << "0.keluar" << endl;
-                        cout << endl;
                         cout << "pilihan : ";
                         cin  >> pilihan;
                     }
